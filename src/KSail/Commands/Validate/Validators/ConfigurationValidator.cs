88caf88e106d7443b81f80f84af3a788aeddf82d
Nikolai Emil Damm <ned@devantler.com>
2025-04-10 15:52:18 +0200

    Refactor validation logic and add unit tests for ConfigurationValidator (#823)
    * refactor: Simplify validation logic in ConfigurationValidator and KSailValidateCommandHandler
    
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>
    
    * test: Add unit tests for ConfigurationValidator to validate distribution and context name
    
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>
    
    ---------
    
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- src/KSail/Commands/Validate/Validators/ConfigurationValidator.cs
+++ src/KSail/Commands/Validate/Validators/ConfigurationValidator.cs
@@ -10,81 +10,70 @@ namespace KSail.Commands.Validate.Validators;
 
 class ConfigurationValidator(KSailCluster config)
 {
-  public async Task<(bool, string)> ValidateAsync(string path, CancellationToken cancellationToken = default)
+  public async Task ValidateAsync(string path, CancellationToken cancellationToken = default)
   {
     string projectRootPath = path;
     try
     {
-      while (!File.Exists(Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)))
-      {
-        projectRootPath = Directory.GetParent(projectRootPath)?.FullName ?? throw new KSailException($"not a valid ksail project directory or subdirectory: '{path}'.");
-      }
+      projectRootPath = GetProjectRootPath(path, projectRootPath);
     }
     catch (Exception)
     {
-      return (true, string.Empty);
+      return;
     }
 
-    var (isValid, message) = CheckContextName(projectRootPath, config.Spec.Project.Distribution, config.Metadata.Name, config.Spec.Connection.Context);
-    if (!isValid)
-      return (false, message);
-
-
-    (isValid, message) = CheckOCISourceUri(projectRootPath, config.Spec.Project.Distribution);
-    if (!isValid)
-      return (false, message);
+    CheckContextName(projectRootPath, config.Spec.Project.Distribution, config.Metadata.Name, config.Spec.Connection.Context);
+    CheckOCISourceUri(projectRootPath, config.Spec.Project.Distribution);
 
     var deserializer = new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
-    if (config.Spec.Project.Distribution == KSailDistributionType.K3s)
+    switch (config.Spec.Project.Distribution)
     {
-      var distributionConfig = deserializer.Deserialize<K3dConfig>(await File.ReadAllTextAsync(Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath), cancellationToken).ConfigureAwait(false));
+      case KSailDistributionType.K3s:
+        {
+          var distributionConfig = deserializer.Deserialize<K3dConfig>(await File.ReadAllTextAsync(Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath), cancellationToken).ConfigureAwait(false));
+          CheckClusterName(projectRootPath, config.Metadata.Name, distributionConfig.Metadata.Name);
+          CheckK3dCNI(projectRootPath, distributionConfig);
+          CheckK3dMirrorRegistries(projectRootPath, distributionConfig);
+          break;
+        }
 
-      (isValid, message) = CheckClusterName(projectRootPath, config.Metadata.Name, distributionConfig.Metadata.Name);
-      if (!isValid)
-        return (false, message);
+      case KSailDistributionType.Native:
+        {
+          var distributionConfig = deserializer.Deserialize<KindConfig>(await File.ReadAllTextAsync(Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath), cancellationToken).ConfigureAwait(false));
+          CheckClusterName(projectRootPath, config.Metadata.Name, distributionConfig.Name);
+          CheckKindCNI(projectRootPath, distributionConfig);
+          break;
+        }
 
-      (isValid, message) = CheckK3dCNI(projectRootPath, distributionConfig);
-      if (!isValid)
-        return (false, message);
-
-      (isValid, message) = CheckK3dMirrorRegistries(projectRootPath, distributionConfig);
-      if (!isValid)
-        return (false, message);
+      default:
+        throw new KSailException($"unsupported distribution '{config.Spec.Project.Distribution}'.");
     }
-    else if (config.Spec.Project.Distribution == KSailDistributionType.Native)
-    {
-      var distributionConfig = deserializer.Deserialize<KindConfig>(await File.ReadAllTextAsync(Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath), cancellationToken).ConfigureAwait(false));
-      (isValid, message) = CheckClusterName(projectRootPath, config.Metadata.Name, distributionConfig.Name);
-      if (!isValid)
-        return (false, message);
-
-      (isValid, message) = CheckKindCNI(projectRootPath, distributionConfig);
-      if (!isValid)
-        return (false, message);
-    }
-    else
-    {
-      throw new KSailException($"unsupported distribution '{config.Spec.Project.Distribution}'.");
-    }
-    return (true, string.Empty);
   }
 
-  (bool isValid, string message) CheckK3dMirrorRegistries(string projectRootPath, K3dConfig distributionConfig)
+  string GetProjectRootPath(string path, string projectRootPath)
   {
-    // check that k3d config includes all mirrors from the ksail config
-    var expectedMirrors = config.Spec.MirrorRegistries.Select(x => x.Proxy.Url.Host.Contains("docker.io", StringComparison.Ordinal) ? "docker.io" : x.Proxy.Url.Host);
-    foreach (string? expectedMirror in expectedMirrors)
+    while (!File.Exists(Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)))
     {
-      if (!(distributionConfig.Registries?.Config?.Contains(expectedMirror, StringComparison.Ordinal) ?? false))
+      projectRootPath = Directory.GetParent(projectRootPath)?.FullName ?? throw new KSailException($"not a valid ksail project directory or subdirectory: '{path}'.");
+    }
+
+    return projectRootPath;
+  }
+
+  void CheckK3dMirrorRegistries(string projectRootPath, K3dConfig distributionConfig)
+  {
+    var expectedMirrors = config.Spec.MirrorRegistries.Select(x => x.Proxy.Url.Host.Contains("docker.io", StringComparison.Ordinal) ? "docker.io" : x.Proxy.Url.Host) ?? [];
+    foreach (string expectedMirror in expectedMirrors)
+    {
+      if (distributionConfig.Registries?.Config?.Contains(expectedMirror, StringComparison.Ordinal) != true)
       {
-        return (false, $"'registries.config' in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}' does not contain the expected mirror '{expectedMirror}'." + Environment.NewLine +
+        throw new KSailException($"'registries.config' in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}' does not contain the expected mirror '{expectedMirror}'." + Environment.NewLine +
           $"  - please add the mirror to 'registries.config' in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'.");
       }
     }
-    return (true, string.Empty);
   }
 
-  (bool isValid, string message) CheckContextName(string projectRootPath, KSailDistributionType distribution, string name, string context)
+  void CheckContextName(string projectRootPath, KSailDistributionType distribution, string name, string context)
   {
     string expectedContextName = distribution switch
     {
@@ -92,12 +81,13 @@ class ConfigurationValidator(KSailCluster config)
       KSailDistributionType.Native => $"kind-{name}",
       _ => throw new KSailException($"unsupported distribution '{distribution}'.")
     };
-    return !string.Equals(expectedContextName, context, StringComparison.Ordinal)
-      ? (false, $"'config.spec.connection.context' in '{Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)}' does not match the expected value '{expectedContextName}'.")
-      : (true, string.Empty);
+    if (!string.Equals(expectedContextName, context, StringComparison.Ordinal))
+    {
+      throw new KSailException($"'config.spec.connection.context' in '{Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)}' does not match the expected value '{expectedContextName}'.");
+    }
   }
 
-  (bool isValid, string message) CheckOCISourceUri(string projectRootPath, KSailDistributionType distribution)
+  void CheckOCISourceUri(string projectRootPath, KSailDistributionType distribution)
   {
     var expectedOCISourceUri = distribution switch
     {
@@ -105,20 +95,22 @@ class ConfigurationValidator(KSailCluster config)
       KSailDistributionType.K3s => new Uri("oci://host.k3d.internal:5555/ksail-registry"),
       _ => throw new KSailException($"unsupported distribution '{distribution}'.")
     };
-    return !Equals(expectedOCISourceUri, config.Spec.DeploymentTool.Flux.Source.Url)
-      ? (false, $"'config.spec.deploymentTool.flux.source.url' in '{Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)}' does not match the expected value '{expectedOCISourceUri}'.")
-      : (true, string.Empty);
+    if (!Equals(expectedOCISourceUri, config.Spec.DeploymentTool.Flux.Source.Url))
+    {
+      throw new KSailException($"'config.spec.deploymentTool.flux.source.url' in '{Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)}' does not match the expected value '{expectedOCISourceUri}'.");
+    }
   }
 
-  (bool, string) CheckClusterName(string projectRootPath, string ksailClusterName, string distributionClusterName)
+  void CheckClusterName(string projectRootPath, string ksailClusterName, string distributionClusterName)
   {
-    return !string.Equals(ksailClusterName, distributionClusterName, StringComparison.Ordinal)
-      ? (false, $"'metadata.name' in '{Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)}' does not match cluster name in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'." + Environment.NewLine +
-        $"  - please set cluster name to '{ksailClusterName}' in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'.")
-      : (true, string.Empty);
+    if (!string.Equals(ksailClusterName, distributionClusterName, StringComparison.Ordinal))
+    {
+      throw new KSailException($"'metadata.name' in '{Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)}' does not match cluster name in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'." + Environment.NewLine +
+        $"  - please set cluster name to '{ksailClusterName}' in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'.");
+    }
   }
 
-  (bool, string) CheckK3dCNI(string projectRootPath, K3dConfig distributionConfig)
+  void CheckK3dCNI(string projectRootPath, K3dConfig distributionConfig)
   {
     var expectedWithCustomCNIK3sExtraArgs = new List<K3dOptionsK3sExtraArg>
       {
@@ -141,29 +133,27 @@ class ConfigurationValidator(KSailCluster config)
     var actual = distributionConfig.Options?.K3s?.ExtraArgs?.Select(x => x.Arg + ":" + (x.NodeFilters?.First() ?? "server:*")) ?? [];
     if (config.Spec.Project.CNI == KSailCNIType.Default && actual.Intersect(expectedWithCustomCNI).Any())
     {
-      return (false, $"'spec.project.cni={config.Spec.Project.CNI}' in '{Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)}' does not match expected values in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'." + Environment.NewLine +
+      throw new KSailException($"'spec.project.cni={config.Spec.Project.CNI}' in '{Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)}' does not match expected values in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'." + Environment.NewLine +
         $"  - please remove '--flannel-backend=none' and '--disable-network-policy' from 'options.k3s.extraArgs' in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'.");
     }
-    else if (config.Spec.Project.CNI != KSailCNIType.Default && !actual.All(expectedWithCustomCNI.Contains))
+    else if (config.Spec.Project.CNI != KSailCNIType.Default && (!actual.Any() || !actual.All(expectedWithCustomCNI.Contains)))
     {
-      return (false, $"'spec.project.cni={config.Spec.Project.CNI}' in '{Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)}' does not match expected values in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'." + Environment.NewLine +
+      throw new KSailException($"'spec.project.cni={config.Spec.Project.CNI}' in '{Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)}' does not match expected values in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'." + Environment.NewLine +
         $"  - please set 'options.k3s.extraArgs' to '--flannel-backend=none' and '--disable-network-policy' for 'server:*' in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'.");
     }
-    return (true, string.Empty);
   }
 
-  (bool isValid, string message) CheckKindCNI(string projectRootPath, KindConfig distributionConfig)
+  void CheckKindCNI(string projectRootPath, KindConfig distributionConfig)
   {
     if (config.Spec.Project.CNI == KSailCNIType.Default && distributionConfig.Networking?.DisableDefaultCNI == true)
     {
-      return (false, $"'spec.project.cni={config.Spec.Project.CNI}' in '{Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)}' does not match expected values in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'." + Environment.NewLine +
+      throw new KSailException($"'spec.project.cni={config.Spec.Project.CNI}' in '{Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)}' does not match expected values in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'." + Environment.NewLine +
         $"  - please set 'networking.disableDefaultCNI: false' in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'.");
     }
     else if (config.Spec.Project.CNI != KSailCNIType.Default && distributionConfig.Networking?.DisableDefaultCNI != true)
     {
-      return (false, $"'spec.project.cni={config.Spec.Project.CNI}' in '{Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)}' does not match expected values in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'." + Environment.NewLine +
+      throw new KSailException($"'spec.project.cni={config.Spec.Project.CNI}' in '{Path.Combine(projectRootPath, config.Spec.Project.ConfigPath)}' does not match expected values in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'." + Environment.NewLine +
         $"  - please set 'networking.disableDefaultCNI: true' in '{Path.Combine(projectRootPath, config.Spec.Project.DistributionConfigPath)}'.");
     }
-    return (true, string.Empty);
   }
 }
