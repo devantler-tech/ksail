6f4f7b2606fe2d162643da2240b5fec840f11c70
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-04-19 21:16:08 +0200

    fix: add additional socket path check in GetPodmanSocket for improved compatibility
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- src/KSail/Utils/PodmanHelper.cs
+++ src/KSail/Utils/PodmanHelper.cs
@@ -6,6 +6,7 @@ class PodmanHelper
   {
     return File.Exists($"/run/user/{Environment.GetEnvironmentVariable("EUID")}/podman/podman.sock") ?
       $"unix:///run/user/${Environment.GetEnvironmentVariable("EUID")}/podman/podman.sock" : File.Exists($"/run/user/{Environment.GetEnvironmentVariable("UID")}/podman/podman.sock") ?
-      $"unix:///run/user/${Environment.GetEnvironmentVariable("UID")}/podman/podman.sock" : "unix:///var/run/docker.sock";
+      $"unix:///run/user/${Environment.GetEnvironmentVariable("UID")}/podman/podman.sock" : File.Exists("/run/podman/podman.sock") ?
+      "unix:///run/podman/podman.sock" : "unix:///var/run/docker.sock";
   }
 }
