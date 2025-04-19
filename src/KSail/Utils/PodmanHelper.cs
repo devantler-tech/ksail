b1e230d115820821d306a8867552373f030acfe3
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-04-19 21:13:04 +0200

    fix: remove Windows socket handling from GetPodmanSocket for platform consistency
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- src/KSail/Utils/PodmanHelper.cs
+++ src/KSail/Utils/PodmanHelper.cs
@@ -4,8 +4,7 @@ class PodmanHelper
 {
   internal static string GetPodmanSocket()
   {
-    return RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ?
-      "npipe://./pipe/docker_engine" : File.Exists($"/run/user/{Environment.GetEnvironmentVariable("EUID")}/podman/podman.sock") ?
+    return File.Exists($"/run/user/{Environment.GetEnvironmentVariable("EUID")}/podman/podman.sock") ?
       $"unix:///run/user/${Environment.GetEnvironmentVariable("EUID")}/podman/podman.sock" : File.Exists($"/run/user/{Environment.GetEnvironmentVariable("UID")}/podman/podman.sock") ?
       $"unix:///run/user/${Environment.GetEnvironmentVariable("UID")}/podman/podman.sock" : "unix:///var/run/docker.sock";
   }
