ba1100f426786d34c523808926b766523e4f1f48
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-04-19 17:23:54 +0200

    feat: refactor ClusterProvisionerFactory to enhance Podman support and improve provider handling
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- src/KSail/Factories/ClusterProvisionerFactory.cs
+++ src/KSail/Factories/ClusterProvisionerFactory.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using Devantler.KubernetesProvisioner.Cluster.Core;
 using Devantler.KubernetesProvisioner.Cluster.K3d;
 using Devantler.KubernetesProvisioner.Cluster.Kind;
@@ -10,11 +11,31 @@ class ClusterProvisionerFactory
 {
   internal static IKubernetesClusterProvisioner Create(KSailCluster config)
   {
-    return (config.Spec.Project.Provider, config.Spec.Project.Distribution) switch
+    switch (config.Spec.Project.Provider)
     {
-      (KSailProviderType.Docker or KSailProviderType.Podman, KSailDistributionType.Native) => new KindProvisioner(),
-      (KSailProviderType.Docker or KSailProviderType.Podman, KSailDistributionType.K3s) => new K3dProvisioner(),
-      _ => throw new NotSupportedException($"The distribution '{config.Spec.Project.Distribution}' is not supported.")
+      case KSailProviderType.Podman:
+        string dockerHost = Environment.GetEnvironmentVariable("DOCKER_HOST") ?? "unix:var/run/docker.sock";
+        string podmanSocket = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && File.Exists(@"\\.\pipe\docker_engine") ?
+          "npipe://./pipe/docker_engine" : File.Exists($"/run/podman/podman.sock") ?
+          $"unix:///run/podman/podman.sock" : File.Exists($"/run/user/{Environment.GetEnvironmentVariable("EUID")}/podman/podman.sock") ?
+          $"unix:///run/user/{Environment.GetEnvironmentVariable("EUID")}/podman/podman.sock" : File.Exists($"/run/user/{Environment.GetEnvironmentVariable("UID")}/podman/podman.sock") ?
+          $"unix:///run/user/{Environment.GetEnvironmentVariable("UID")}/podman/podman.sock" : dockerHost;
+        Environment.SetEnvironmentVariable("DOCKER_HOST", podmanSocket);
+        return GetClusterProvisioner(config);
+      case KSailProviderType.Docker:
+        return GetClusterProvisioner(config);
+      default:
+        throw new NotSupportedException($"The provider '{config.Spec.Project.Provider}' is not supported.");
+    }
+  }
+
+  static IKubernetesClusterProvisioner GetClusterProvisioner(KSailCluster config)
+  {
+    return config.Spec.Project.Distribution switch
+    {
+      KSailDistributionType.Native => new KindProvisioner(),
+      KSailDistributionType.K3s => new K3dProvisioner(),
+      _ => throw new NotSupportedException($"The distribution '{config.Spec.Project.Distribution}' is not supported."),
     };
   }
 }
