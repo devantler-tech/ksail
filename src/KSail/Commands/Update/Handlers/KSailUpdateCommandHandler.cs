12093e004ab2575cfc456d29223a7dda534b582e
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-05-11 15:59:27 +0200

    refactor: streamline deployment tool initialization in KSailUpdateCommandHandler
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- src/KSail/Commands/Update/Handlers/KSailUpdateCommandHandler.cs
+++ src/KSail/Commands/Update/Handlers/KSailUpdateCommandHandler.cs
@@ -15,21 +15,10 @@ class KSailUpdateCommandHandler : ICommandHandler
   readonly IDeploymentToolProvisioner _deploymentTool;
   readonly KSailCluster _config;
   readonly KSailValidateCommandHandler _ksailValidateCommandHandler;
-  readonly Uri _ociRegistryFromHost;
   internal KSailUpdateCommandHandler(KSailCluster config)
   {
     _ksailValidateCommandHandler = new KSailValidateCommandHandler(config, "./");
-    string scheme = config.Spec.DeploymentTool.Flux.Source.Url.Scheme;
-    string host = "localhost";
-    int port = config.Spec.LocalRegistry.HostPort;
-    string absolutePath = config.Spec.DeploymentTool.Flux.Source.Url.AbsolutePath;
-    _ociRegistryFromHost = new Uri($"{scheme}://{host}:{port}{absolutePath}");
-    _deploymentTool = config.Spec.Project.DeploymentTool switch
-    {
-      KSailDeploymentToolType.Kubectl => new KubectlProvisioner(config.Spec.Connection.Kubeconfig, config.Spec.Connection.Context),
-      KSailDeploymentToolType.Flux => new FluxProvisioner(_ociRegistryFromHost, config.Spec.Connection.Kubeconfig, config.Spec.Connection.Context),
-      _ => throw new NotSupportedException($"The deployment tool '{config.Spec.Project.DeploymentTool}' is not supported.")
-    };
+    _deploymentTool = DeploymentToolProvisionerFactory.Create(config);
     _config = config;
   }
 
@@ -39,7 +28,7 @@ class KSailUpdateCommandHandler : ICommandHandler
     {
       return 1;
     }
-    string manifestDirectory = config.Spec.Project.KustomizationPath
+    string manifestDirectory = _config.Spec.Project.KustomizationPath
       .Replace("./", string.Empty, StringComparison.OrdinalIgnoreCase)
       .Split('/', StringSplitOptions.RemoveEmptyEntries).First();
     if (_config.Spec.Publication.PublishOnUpdate)
@@ -51,17 +40,17 @@ class KSailUpdateCommandHandler : ICommandHandler
       Console.WriteLine(_config.Spec.Project.DeploymentTool switch
       {
         KSailDeploymentToolType.Kubectl => $"⤴️ Applying manifests from '{_config.Spec.Project.KustomizationPath}'",
-        KSailDeploymentToolType.Flux or KSailDeploymentToolType.ArgoCD => $"📥 Pushing manifests to '{_ociRegistryFromHost}'",
+        KSailDeploymentToolType.Flux or KSailDeploymentToolType.ArgoCD => $"📥 Pushing manifests to 'ksail-registry'",
         _ => throw new NotSupportedException($"The deployment tool '{_config.Spec.Project.DeploymentTool}' is not supported.")
       });
       await _deploymentTool.PushAsync(manifestDirectory, _config.Spec.Connection.Timeout, cancellationToken: cancellationToken).ConfigureAwait(false);
     }
 
-    if (config.Spec.Validation.ReconcileOnUpdate)
+    if (_config.Spec.Validation.ReconcileOnUpdate)
     {
       Console.WriteLine();
       Console.WriteLine("🔄 Reconciling changes...");
-      await _deploymentTool.ReconcileAsync(manifestDirectory, config.Spec.Connection.Timeout, cancellationToken).ConfigureAwait(false);
+      await _deploymentTool.ReconcileAsync(manifestDirectory, _config.Spec.Connection.Timeout, cancellationToken).ConfigureAwait(false);
       Console.WriteLine("✔ reconciliation completed");
     }
 
@@ -70,7 +59,7 @@ class KSailUpdateCommandHandler : ICommandHandler
 
   async Task<bool> Validate(CancellationToken cancellationToken = default)
   {
-    if (config.Spec.Validation.ValidateOnUpdate)
+    if (_config.Spec.Validation.ValidateOnUpdate)
     {
       bool success = await _ksailValidateCommandHandler.HandleAsync(cancellationToken).ConfigureAwait(false) == 0;
       Console.WriteLine();
