821edc6702277f274b484a3cba1c52f932e358ac
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-06-28 14:39:39 +0200

    refactor: Update CLIOptionsGeneratorTests for improved whitespace handling and normalization
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- tests/KSail.Docs.Tests.Unit/CLIOptionsGeneratorTests.cs
+++ tests/KSail.Docs.Tests.Unit/CLIOptionsGeneratorTests.cs
@@ -5,11 +5,12 @@ using System.Text.Json.Nodes;
 using System.Text.Json.Schema;
 using System.Text.Json.Serialization;
 using System.Text.Json.Serialization.Metadata;
+using System.Text.RegularExpressions;
 
 namespace KSail.Docs.Tests.Unit;
 
 
-public class CLIOptionsGeneratorTests
+public partial class CLIOptionsGeneratorTests
 {
   [Fact]
   public async Task GenerateCLIOptions_ShouldReturnCLIOptionsMarkdown()
@@ -23,7 +24,17 @@ public class CLIOptionsGeneratorTests
 
     // Assert
     _ = await Verify(expectedMarkdown.ToString(), extension: "md").UseFileName("cli-options");
-    Assert.Equal(expectedMarkdown, actualMarkdown);
+    Assert.Equal(
+      ReplaceWhitespace(Normalize(expectedMarkdown), ""),
+      ReplaceWhitespace(Normalize(actualMarkdown), "")
+    );
   }
+
+  static readonly Regex _sWhitespace = WhiteSpaceRegex();
+  public static string ReplaceWhitespace(string input, string replacement) => _sWhitespace.Replace(input, replacement);
+
+  static string Normalize(string s) => s.Replace("\r", "", StringComparison.Ordinal).Replace("\n", "", StringComparison.Ordinal);
+  [GeneratedRegex(@"\s+")]
+  private static partial Regex WhiteSpaceRegex();
 }
 
