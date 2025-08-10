af68136b0d7714470e062e573899cae4009ffadb
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-08-10 21:44:14 +0200

    refactor: simplify init command by utilizing shared configuration and improving function naming
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- ksail-go/cmd/init.go
+++ ksail-go/cmd/init.go
@@ -3,18 +3,10 @@ package cmd
 import (
 	"fmt"
 
+	"devantler.tech/ksail/cmd/shared"
 	"devantler.tech/ksail/internal/util"
 	ksailcluster "devantler.tech/ksail/pkg/apis/v1alpha1/cluster"
 	"github.com/spf13/cobra"
-)
-
-var (
-	initName               string                          = "ksail-default"
-	initDistribution       ksailcluster.Distribution       = ksailcluster.DistributionKind
-	initReconciliationTool ksailcluster.ReconciliationTool = ksailcluster.ReconciliationToolKubectl
-	initOutput             string                          = "./"
-	initSrcDir             string                          = "k8s"
-	initForce              bool                            = false
 )
 
 var initCmd = &cobra.Command{
@@ -29,43 +21,44 @@ var initCmd = &cobra.Command{
   - 'k8s/kustomization.yaml' as an entry point for Kustomize
   `,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return HandleInit()
+		return handleInit()
 	},
 }
 
-// HandleInit handles the init command.
-func HandleInit() error {
+// --- internals ---
+
+// handleInit handles the init command.
+func handleInit() error {
 	ksailConfig := ksailcluster.NewCluster()
-	SetInitialValuesFromInput(ksailConfig)
-	return Scaffold(ksailConfig)
+	setInitialValuesFromInput(ksailConfig)
+	return scaffold(ksailConfig)
 }
 
-// Scaffold generates initial project files according to the provided configuration.
-func Scaffold(ksailConfig *ksailcluster.Cluster) error {
+// scaffold generates initial project files according to the provided configuration.
+func scaffold(ksailConfig *ksailcluster.Cluster) error {
 	scaffolder := util.NewScaffolder(*ksailConfig)
 	fmt.Println("📝 Scaffolding new project...")
-	if err := scaffolder.Scaffold(initOutput, initForce); err != nil {
+	if err := scaffolder.Scaffold(shared.Output, shared.Force); err != nil {
 		return err
 	}
 	fmt.Println("✔ project scaffolded")
 	return nil
 }
 
-// TODO: Move SetInitialValuesFromInput to a more fitting file
-// SetInitialValuesFromInput mutates clusterObj with CLI-provided values.
-func SetInitialValuesFromInput(ksailConfig *ksailcluster.Cluster) {
-	ksailConfig.Metadata.Name = initName
-	ksailConfig.Spec.Distribution = initDistribution
-	ksailConfig.Spec.ReconciliationTool = initReconciliationTool
-	ksailConfig.Spec.SourceDirectory = initSrcDir
+// setInitialValuesFromInput mutates ksailConfig with CLI-provided values.
+func setInitialValuesFromInput(ksailConfig *ksailcluster.Cluster) {
+	ksailConfig.Metadata.Name = shared.Name
+	ksailConfig.Spec.Distribution = shared.Distribution
+	ksailConfig.Spec.ReconciliationTool = shared.ReconciliationTool
+	ksailConfig.Spec.SourceDirectory = shared.SourceDirectory
 }
 
+// init initializes the init command.
 func init() {
 	rootCmd.AddCommand(initCmd)
-	initCmd.Flags().StringVarP(&initOutput, "output", "o", "./", "output directory")
-	initCmd.Flags().StringVarP(&initName, "name", "n", "ksail-default", "name of cluster")
-	initCmd.Flags().VarP(&initDistribution, "distribution", "d", "distribution to use")
-	initCmd.Flags().VarP(&initReconciliationTool, "reconciliation-tool", "r", "reconciliation tool to use")
-	initCmd.Flags().StringVarP(&initSrcDir, "source-directory", "", "k8s", "manifests source directory")
-	initCmd.Flags().BoolVarP(&initForce, "force", "f", false, "overwrite files")
+	shared.AddNameFlag(initCmd)
+	shared.AddDistributionFlag(initCmd)
+	shared.AddReconciliationToolFlag(initCmd)
+	shared.AddSourceDirectoryFlag(initCmd)
+	shared.AddForceFlag(initCmd)
 }
