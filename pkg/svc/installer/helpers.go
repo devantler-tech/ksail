b226dd704e3d5e2b67463b617889f793cea1819b
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2026-01-07 21:01:02 +0100

    fix: reduce default and Talos installation timeouts to improve feedback
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- pkg/svc/installer/helpers.go
+++ pkg/svc/installer/helpers.go
@@ -8,11 +8,10 @@ import (
 
 const (
 	// DefaultInstallTimeout is the default timeout for component installation.
-	DefaultInstallTimeout = 5 * time.Minute
+	DefaultInstallTimeout = 3 * time.Minute
 	// TalosInstallTimeout is the default timeout for Talos component installation.
-	// Talos clusters take longer to bootstrap due to the immutable OS design,
-	// so we use a longer timeout (10 minutes) per operation.
-	TalosInstallTimeout = 10 * time.Minute
+	// Talos clusters take longer to bootstrap due to the immutable OS design.
+	TalosInstallTimeout = 5 * time.Minute
 )
 
 // GetInstallTimeout determines the timeout for component installation.
