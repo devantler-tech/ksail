d115aeb71f57b48a383862d743da93f8306b4f81
devantler <26203420+devantler@users.noreply.github.com>
2026-02-22 23:10:03 +0000

    chore: apply golangci-lint fixes

--- pkg/cli/setup/mirrorregistry/defaults.go
+++ pkg/cli/setup/mirrorregistry/defaults.go
@@ -56,6 +56,7 @@ func GetMirrorRegistriesWithDefaults(
 // Returns empty slice if user explicitly disabled mirrors with empty string.
 func getFlagMirrors(cmd *cobra.Command) []string {
 	flagValues, _ := cmd.Flags().GetStringSlice(MirrorRegistryFlag)
+
 	return flagValues
 }
 
